825a2c38e349169bea498af8f3a2e7523100400e
portableclass <av.yashutkin@gmail.com>
2021-12-21 15:18:49 +0300

    0.10

--- src/main.cpp
+++ src/main.cpp
@@ -46,40 +46,40 @@ int main(int argc, char* argv[]) {
             getline(outputFile, outputText);
             std::cout << outputText;
 
-//            // 0. Установка пути для сохранения данных:
-//            const std::string way = "../resources/";
-//            const std::string filename = "data_" + getTime();
-//            const std::string fullwayname = way + filename + ".txt";
-//
-//            // 1. Инциализация пустых векторов данных для хранения результатов рассчёта:
-//            DataVector x_data_Newton("array of roots Newton's method");
-//            DataVector delta_data_Newton("array of error estimates Newton's method");
-//
-//            DataVector x_data_NewtonFinite("array of roots of finite Newton's method");
-//            DataVector delta_data_NewtonFinite("array of error estimates of finite Newton's method");
-//
-//            // 2. Задание параметров для процесса уточнения корня меодом Ньютона:
-//            const double x_start = 179.0156;
-//            const unsigned int mantissa_size = 64;
-//            const double delta_max = 1e-32;
-//            const unsigned int iter_max = 1e2;
-//
-//            // 3. Вызов метода Ньютона:
-//            const double root_Newton = solveNewton(x_start, fun_, D1fun_, x_data_Newton, delta_data_Newton, delta_max, iter_max);
-//
-//            // 4. Вызов конечно-разностного метода Ньютона:
-//            const double root_NewtonFinite = solveNewtonFinite(x_start, fun_, x_data_NewtonFinite, delta_data_NewtonFinite, delta_max, iter_max);
-//
-//            // 5. Вывод результатов:
-//            std::printf("\n\nNewton's method :\n		root is %1.15e\n		iteration number is %i\n", root_Newton, x_data_Newton.size());
-//            std::printf("\n\nfinite Newton's method :\n		root is %1.15e\n		iteration number is %i\n", root_NewtonFinite, x_data_NewtonFinite.size());
-//            std::printf("\n||x_Newton - x_Newton.f|| = %1.31e\n", std::abs(root_Newton - root_NewtonFinite));
-//
-//            // 6. Сохранение результатов в файл:
-//            x_data_Newton.Save(fullwayname, mantissa_size);
-//            delta_data_Newton.Save(fullwayname, mantissa_size);
-//            x_data_NewtonFinite.Save(fullwayname, mantissa_size);
-//            delta_data_NewtonFinite.Save(fullwayname, mantissa_size);
+            // 0. Установка пути для сохранения данных:
+            const std::string way = "../resources/";
+            const std::string filename = "data_" + getTime();
+            const std::string fullwayname = way + filename + ".txt";
+
+            // 1. Инциализация пустых векторов данных для хранения результатов рассчёта:
+            DataVector x_data_Newton("array of roots Newton's method");
+            DataVector delta_data_Newton("array of error estimates Newton's method");
+
+            DataVector x_data_NewtonFinite("array of roots of finite Newton's method");
+            DataVector delta_data_NewtonFinite("array of error estimates of finite Newton's method");
+
+            // 2. Задание параметров для процесса уточнения корня меодом Ньютона:
+            const double x_start = 179.0156;
+            const unsigned int mantissa_size = 64;
+            const double delta_max = 1e-32;
+            const unsigned int iter_max = 1e2;
+
+            // 3. Вызов метода Ньютона:
+            const double root_Newton = solveNewton(x_start, fun_, D1fun_, x_data_Newton, delta_data_Newton, delta_max, iter_max);
+
+            // 4. Вызов конечно-разностного метода Ньютона:
+            const double root_NewtonFinite = solveNewtonFinite(x_start, fun_, x_data_NewtonFinite, delta_data_NewtonFinite, delta_max, iter_max);
+
+            // 5. Вывод результатов:
+            std::printf("\n\nNewton's method :\n		root is %1.15e\n		iteration number is %i\n", root_Newton, x_data_Newton.size());
+            std::printf("\n\nfinite Newton's method :\n		root is %1.15e\n		iteration number is %i\n", root_NewtonFinite, x_data_NewtonFinite.size());
+            std::printf("\n||x_Newton - x_Newton.f|| = %1.31e\n", std::abs(root_Newton - root_NewtonFinite));
+
+            // 6. Сохранение результатов в файл:
+            x_data_Newton.Save(fullwayname, mantissa_size);
+            delta_data_Newton.Save(fullwayname, mantissa_size);
+            x_data_NewtonFinite.Save(fullwayname, mantissa_size);
+            delta_data_NewtonFinite.Save(fullwayname, mantissa_size);
         }
         else {
             std::cout << "Error: " << outputFile.rdstate();
